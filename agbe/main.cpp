d8b7e3aeec0db23445c293aa758c08c75f23c160
Henry Southall <hen0104.hs@gmail.com>
2022-06-02 19:26:18 +0100

    add (currently useless) emulation thread

--- agbe/main.cpp
+++ agbe/main.cpp
@@ -2,6 +2,9 @@
 #include"Display.h"
 
 #include<iostream>
+#include<thread>
+
+void emuWorkerThread();
 
 int main()
 {
@@ -11,11 +14,24 @@ int main()
 	//ppu can be polled whenever necessary pretty simply, by having some vfunc to return a framebuffer - uploaded to display
 	Display m_display(4);
 
+	std::thread m_workerThread(&emuWorkerThread);
+
 	while (!m_display.getShouldClose())
 	{
 		//update texture
 		m_display.draw();
 	}
 
+	m_workerThread.join();
+
 	return 0;
+}
+
+void emuWorkerThread()
+{
+	Logger::getInstance()->msg(LoggerSeverity::Info, "Entered worker thread!!");
+	int abcd = 0;
+	for (abcd = 0; abcd < 100; abcd++)
+		(void)0;
+	Logger::getInstance()->msg(LoggerSeverity::Info, "Exited worker thread!!");
 }
