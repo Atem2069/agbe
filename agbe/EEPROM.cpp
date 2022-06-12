3a960c2c88540d1df03a1d093efc1c01e616a68b
Henry Southall <hen0104.hs@gmail.com>
2022-06-12 02:29:54 +0100

    rewrite eeprom so it's way more elegant. still no support for tiny eeproms but it's better!

--- agbe/EEPROM.cpp
+++ agbe/EEPROM.cpp
@@ -2,7 +2,8 @@
 
 EEPROM::EEPROM()
 {
-
+	state = WriteState::RequestType;
+	readbackCount = 0;
 }
 
 EEPROM::~EEPROM()
@@ -12,66 +13,90 @@ EEPROM::~EEPROM()
 
 uint8_t EEPROM::read(uint32_t address)
 {
-	uint8_t readData = 1;
-	if (!isRead)
+	uint8_t retData = 1;
+	if (!isReading || !activeRead)
 		return 1;
-	if (isRead && readbackProgress < 68 && readbackProgress >= 4)
+
+	readbackCount++;
+	if (readbackCount > 4)
 	{
-		int shiftIdx = (63 - (readbackProgress-4));
-		readData = (readbackData >> shiftIdx)&0b1;
-		std::cout << "read data" << (int)readData << '\n';
+		retData = (readData >> 63) & 0b1;
+		readData <<= 1;
 	}
 
-	if (readbackProgress == 68)
+	if (readbackCount == 68)
 	{
-		readbackProgress = -1;
-		isRead = false;
+		readbackCount = 0;
+		activeRead = false;
 	}
-
-	readbackProgress++;
-	return (readData & 0b1);
+	return retData;
 }
 
 void EEPROM::write(uint32_t address, uint8_t value)
 {
 	//this whole code sucks, lol
 	value &= 0b1;
-	if (requestProgress < 2)
-		requestData |= (value << requestProgress);
-	if (requestProgress == 1)
+	tempWriteBits <<= 1;
+	tempWriteBits |= (value & 0b1);
+	writeCount++;
+
+	switch (state)
 	{
-		if ((requestData & 0b11) == 3)
-			isRead = true;
-		else
-			isRead = false;
-		requestData = 0;
-	}
-	if (requestProgress >= 2 && requestProgress <= 15)
-		curAddress |= ((uint16_t)value << (13 - (requestProgress - 2)));
-	if ((requestProgress >= 16) && (requestProgress <= 79) && !isRead)
-	{
-		dataToWrite |= ((uint64_t)value << (63 - (requestProgress - 16)));
-		if (requestProgress == 79)
+	case WriteState::RequestType:
+		if (writeCount == 2)
 		{
-			ROMData[curAddress] = dataToWrite;
-			std::cout << "write " << std::hex << (uint32_t)curAddress << " " << std::hex <<  " " << (uint64_t)dataToWrite << '\n';
-			curAddress = 0;
-			dataToWrite = 0;
+			if ((tempWriteBits&0b11) == 3)
+				isReading = true;
+			else
+				isReading = false;
+
+			state = WriteState::Address;
+			tempWriteBits = 0;
+			writeCount = 0;
 		}
-	}
+		break;
+	case WriteState::Address:
+		readAddress |= ((uint16_t)value << (13- (writeCount-1)));
+		if (writeCount == 14)	//todo: account for smaller address
+		{
+			if (readAddress > 1024)
+				readAddress &= 0b1111111111;
+			if (isReading)
+			{
+				readData = ROMData[readAddress];
+				activeRead = true;
+				readbackCount = 0;
+			}
+			else
+				writeAddress = readAddress;
 
-	if ((requestProgress == 16) && isRead)
-	{
-		requestProgress = -1;
-		readbackData = ROMData[curAddress];
-		std::cout << "read req " << std::hex << (uint32_t)curAddress << '\n';
-
-		curAddress = 0;
+			state = WriteState::Data;
+			tempWriteBits = 0;
+			writeCount = 0;
+			readAddress = 0;
+		}
+		break;
+	case WriteState::Data:
+		if (isReading)
+		{
+			state = WriteState::RequestType;
+			tempWriteBits = 0;
+			writeCount = 0;
+		}
+		else
+		{
+			if (writeCount <= 64)
+				newROMData |=  ((uint64_t)value << (63-(writeCount - 1)));
+			if (writeCount == 65)
+			{
+				ROMData[writeAddress] = newROMData;
+				state = WriteState::RequestType;
+				tempWriteBits = 0;
+				newROMData = 0;
+				writeCount = 0;
+				writeAddress = 0;
+			}
+		}
+		break;
 	}
-	if ((requestProgress == 80))
-	{
-		requestProgress = -1;
-	}
-
-	requestProgress++;
 }
--- agbe/EEPROM.h
+++ agbe/EEPROM.h
@@ -4,6 +4,13 @@
 
 #include<iostream>
 
+enum class WriteState
+{
+	RequestType,
+	Address,
+	Data
+};
+
 class EEPROM
 {
 public:
@@ -15,12 +22,24 @@ public:
 	void write(uint32_t address, uint8_t value);
 private:
 	uint64_t ROMData[1024];
-	uint64_t requestData = 0;
-	uint64_t dataToWrite = 0;
-	uint16_t curAddress = 0;
-	int requestProgress = 0;
-	bool isRead = false;
+	//uint64_t requestData = 0;
+	//uint64_t dataToWrite = 0;
+	//uint16_t curAddress = 0;
+	//int requestProgress = 0;
+	//bool isRead = false;
 
-	uint64_t readbackData = 0;
-	int readbackProgress = 0;
+	//uint64_t readbackData = 0;
+	//int readbackProgress = 0;
+
+	uint32_t writeCount = 0;
+	uint32_t readbackCount = 0;
+	uint32_t writeAddress = 0;
+	uint32_t readAddress = 0;
+	uint64_t readData = 0;
+	uint64_t tempWriteBits = 0;
+	uint64_t newROMData = 0;
+	bool isReading = false;
+	bool activeRead = false;
+	WriteState state = {};
+
 };
