80997d9876f491181fc5f7a41abc03e0f1242aa4
Henry Southall <hen0104.hs@gmail.com>
2022-09-23 23:07:37 +0100

    flesh out gpio r/w handlers

--- agbe/GPIO_RTC.cpp
+++ agbe/GPIO_RTC.cpp
@@ -12,9 +12,18 @@ RTC::~RTC()
 
 uint8_t RTC::read(uint32_t address)
 {
+	if (!readWriteMask)
+		return 0x0;
 
-	//todo
-	return 0x0;
+	switch (address)
+	{
+	case 0x080000C4:
+		return (data & (~directionMask)) & 0xF;
+	case 0x080000C6:
+		return directionMask & 0xF;
+	case 0x080000C8:
+		return readWriteMask & 0b1;
+	}
 }
 
 //according to gbatek: rom space writes can only be 16/32 bit..hmm
@@ -25,6 +34,11 @@ void RTC::write16(uint32_t address, uint16_t value)
 
 void RTC::write32(uint32_t address, uint32_t value)
 {
-	//probs just handle by splitting into two 16 bit writes if necessary (e.g. if C4 then C4,C6; if C6 then C6,C8)
-	Logger::getInstance()->msg(LoggerSeverity::Warn, "32 bit write to rtc register...");
+	write16(address, (uint16_t)value & 0xFFFF);
+	switch (address)										//handle 32 bit reads when they need to be split up into separate register reads (i.e. reading port data/direction regs)
+	{
+	case 0x080000C4: case 0x080000C6:
+		write16(address + 2, (uint16_t)value & 0xFFFF);
+		break;
+	}
 }
--- agbe/GPIO_RTC.h
+++ agbe/GPIO_RTC.h
@@ -11,4 +11,9 @@ public:
 	//according to gbatek: rom space writes can only be 16/32 bit..hmm
 	void write16(uint32_t address, uint16_t value);
 	void write32(uint32_t address, uint32_t value);
+
+private:
+	uint8_t data = {};
+	uint8_t directionMask = {};
+	bool readWriteMask = false;
 };
